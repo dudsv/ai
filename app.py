97a84d4bcde95f884cf6699a0226c9bc2e4f2e53
Eduardo Vetromille <85708392+dudsv@users.noreply.github.com>
2025-05-22 03:30:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -50,5 +50,8 @@ def enviar_resposta(phone, message):
 def home():
     return "✅ Chatbot com Z-API + Together AI está ativo."
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
